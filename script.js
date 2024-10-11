39adeb01ca9bd4d215f65c03ae588dcffeb60edf
bog <119977760+aukak@users.noreply.github.com>
2024-10-11 17:59:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function stars() {
   document.body.appendChild(e);
   e.style.left = Math.random() * +innerWidth + "px";
 
-  let size = Math.random() * 12;
+  let size = Math.random() * 19;
+  let stars = Math.random()* 100;
   let duration = Math.random() * 3;
 
-  e.style.fontSize = 12 + "px";
+  e.style.fontSize = 20 + "px";
   e.style.animationDuration = 2 + duration + "s";
   setTimeout(function () {
     document.body.removeChild(e);
